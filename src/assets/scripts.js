dc080d6ff0c004ba79263aaac88bf14c2039d5a0
EmmerichRD <ekoalac17@gmail.com>
2024-10-31 20:25:16 -0400

    added main idea for application

---
+++ src/assets/scripts.js
@@ -0,0 +1,9 @@
+// APP FUNCTIONALITY
+
+/* Have a way to maybe submit different foods and pull data
+from the internet that shows calories (maybe macros) and average food costs
+pulled from a few different popular grocery chains.
+
+
+*/
+
